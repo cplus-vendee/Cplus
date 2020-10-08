075a51e1ab4b2aaf07d65f020c6829ef8732cde3
juju <bardin.julien41@gmail.com>
2020-10-08 11:04:38 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,7 @@ var User= require('mongoose').model('user');
  */
 
 router.post(
-    "/a1z2e3r4t5y6u7i8o9p10q", auth,
+    "/a1z2e3r4t5y6u7i8o9p10q",
     [
         check("username", "Please Enter a Valid Username")
             .not()
